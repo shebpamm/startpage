0fcd12dd9575a536f97d08cf26b2fa56c98c718e
Erik Karsten <shebpamm@gmail.com>
2017-10-26 13:35:15 +0300

    Added partial calendar functionality

--- start_files/cal_init.js
+++ start_files/cal_init.js
@@ -1,13 +1,49 @@
-var vev
+hours = [{s:'0815',e:'0930'},{s:'0940',e:'1100'},{s:'1105',e:'1250'},{s:'1305',e:'1420'},{s:'1430',e:'1545'}]
+
+function appendList(v, h) {
+    $('.table').append('<div class="row">'+v+'</div>');
+}
+
+function filterList(props) {
+    curDate = moment();
+    var todaysList = [];
+    for (var i = 0; i < props.length; i++)
+    {
+        comTime = props[i].starttime;
+        if(props[i].rrule == null) continue;
+        uTime = props[i].rrule.until;
+        classTime = moment(comTime.year + '-' + comTime.month + '-' + comTime.day);
+        untilTime = moment(uTime.year + '-' + uTime.month + '-' + uTime.day);
+        if(classTime.isSameOrBefore(curDate) && classTime.weekday() == curDate.weekday() && curDate.isSameOrBefore(untilTime))
+        {
+            appendList(props[i].name.split(" ")[0]);
+        }
+    }
+    for (var i = 0; i < hours.length; i++) {
+        st = moment(hours[i].s, 'hhmm')
+        et = moment(hours[i].e, 'hhmm')
+        if(st < moment() && moment() < et) $('.row').eq(i).css("color", "#3399FF")
+    }
+
+}
+
 $(document).ready(function() {
   url = "https://wilma.edu.hel.fi/schedule/export/students/546835/Wilma.ics?token=02546835x9ac972ffd350081f3103c9a41f2b271b&p=28&f=56&tstamp=1509006401";
-  var raw_data = $.get(url, function (rdata) {
-    data = raw_data.split('\n');
-    for (var i = 0; i < data.length; i++) {
-      if(data[i][0] == 'S')
-      {
-        vals = data[i].split(' ');
-      }
-    }
+  var req = $.get(url, function (rdata) {
+      var jcal = ICAL.parse(rdata);
+      var vcal = new ICAL.Component(jcal);
+      var vevents = vcal.getAllSubcomponents("vevent");
+      props = _.map(vevents,function(vevent) {
+         return {
+           name: vevent.getFirstPropertyValue("summary"),
+           starttime: vevent.getFirstPropertyValue("dtstart"),
+           endtime: vevent.getFirstPropertyValue("dtend"),
+           description: vevent.getFirstPropertyValue("description"),
+           rrule: vevent.getFirstPropertyValue("rrule")
+           };
+       });
+       console.log(props);
+       filterList(props);
+
     });
-  });
+});
